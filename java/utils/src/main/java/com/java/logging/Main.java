67739544596eb55a88ffa48fd804396440a8871f
brahim <brahim.bahri@gfi.fr>
2017-04-27 16:37:33 +0200

    Logging: tests log4j display

--- java/utils/src/main/java/com/java/logging/Main.java
+++ java/utils/src/main/java/com/java/logging/Main.java
@@ -1,5 +1,8 @@
 package com.java.logging;
 import org.apache.log4j.Logger;
+
+import java.util.NoSuchElementException;
+
 /**
  * @author: brahim bahri
  * date: 04/04/17.
@@ -9,5 +12,17 @@ public class Main {
     public static void main(String[] args){
         logger.info("info message");
         logger.error("error message");
+        try {
+            f();
+        }
+        catch(NoSuchElementException e){
+           logger.error("exception test", e);
+
+        }
+    }
+
+    public static void f() throws NoSuchElementException{
+        throw new NoSuchElementException();
+
     }
 }
