b3a515f013f66acee88ca49e91285c6a34971449
brahim <ibra.bahri@gmail.com>
2017-12-28 15:55:07 +0100

    added QuickServer File System handler for fun

--- coding-tests/tinyclues/src/Counter.scala
+++
@@ -1,6 +0,0 @@
-/**
-  * Created by brahim on 9/13/17.
-  */
-trait Counter {
-  def countDistinct(arr: Array[Long]): Long
-}
--- coding-tests/tinyclues/src/Finder.scala
+++
@@ -1,6 +0,0 @@
-/**
-  * Created by brahim on 9/12/17.
-  */
-trait Finder {
-  def findMissing(arr: Array[Long]):Array[Long]
-}
